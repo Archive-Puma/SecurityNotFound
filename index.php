69282de6f1cfe8b7cd7cfa4f7f3191ab34cfaf3e
A Puma on a Penguin <kikefontanlorenzo@gmail.com>
2018-07-20 18:18:17 +0200

    :man: Headers

--- index.php
+++ index.php
@@ -1,11 +1,17 @@
 <?php
 
-/* Metadata
->>>>>>>>>>>>>>>>> */
-define('__NAME__', 'SecurityNotFound');
-define('__AUTHOR__', '@CosasDePuma');
+  /* Metadata
+  >>>>>>>>>>>>>>>>> */
+  define('__NAME__', 'SecurityNotFound');
+  define('__AUTHOR__', '@CosasDePuma');
 
-/* Variables
->>>>>>>>>>>>>>>>> */
-define('password', 'cosasdepuma')
+  /* Variables
+  >>>>>>>>>>>>>>>>> */
+  define('password', 'cosasdepuma');
+
+  /* Headers
+  >>>>>>>>>>>>>>>>> */
+  header('HTTP/1.0 404 Not Found');
+  header('X-Powered-By: Unicorns and kitties <3');
+
 ?>
