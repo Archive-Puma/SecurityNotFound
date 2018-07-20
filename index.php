ee8787ca0fe5dc3588dec7c18870c3f83e9f85a8
A Puma on a Penguin <kikefontanlorenzo@gmail.com>
2018-07-20 18:19:46 +0200

    :moyai: Robots

--- index.php
+++ index.php
@@ -14,4 +14,14 @@
   header('HTTP/1.0 404 Not Found');
   header('X-Powered-By: Unicorns and kitties <3');
 
+  /* Robots
+  >>>>>>>>>>>>>>>>> */
+  $robots = array(
+    'Google', 'Yandex', 'MSNBot', 'Slurp', 'Rambler', 'ia_archiver'
+  );
+
+  foreach($robots as $robot)
+    if( strpos($_SERVER['HTTP_USER_AGENT'],$robot) !== false )
+      exit;
+
 ?>
